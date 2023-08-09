f10fe2e805a1f31aa7544ae47c9b91c1474679e0
TheMironDCI <shaxobiddinovmironshoh@gmail.com>
2023-08-09 22:07:22 +0500

    kfm

--- src/Everest.js
+++ src/Everest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Section1/Header'
 import Courses from './Section1/Courses';
-import Map from './Section/Map';
+import Map from './Section1/Map';
 
 export default function Everest() {
   return (
